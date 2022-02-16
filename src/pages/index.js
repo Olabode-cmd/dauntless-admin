6b3a70097d01fa5e54e7de05a1fa8dc781723eec
phemieny7 <oyewo.oluwafemi@gmail.com>
2022-02-16 16:04:59 +0100

    go hard

--- src/pages/index.js
+++ src/pages/index.js
@@ -60,7 +60,7 @@ export default function HomePage() {
               <div className="p-4 flex items-center">
                 <div className="p-3 rounded-full text-teal-500 dark:text-teal-100 bg-teal-100 dark:bg-teal-500 mr-4">
                   <svg fill="currentColor" viewBox="0 0 20 20" className="w-5 h-5">
-                    <path fill-rule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clip-rule="evenodd"></path>
+                    <path fillRule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clipRule="evenodd"></path>
                   </svg>
                 </div>
                 <div>
