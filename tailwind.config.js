31f0fbbcee52a18a4bd13851065f6a25f4838881
phemieny7 <oyewo.oluwafemi@gmail.com>
2022-03-26 10:33:11 +0100

    go on

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-<<<<<<< HEAD
-  content: ['./src/**/*.{js,jsx,ts,tsx}',  "./node_modules/flowbite/**/*.js"],
-=======
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
->>>>>>> febb7e4480851872c6faa30e8ba9f9315a3017f6
   darkMode: "class",
   mode: 'jit',
   theme: {
@@ -12,11 +8,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-<<<<<<< HEAD
-  plugins: [
-    require("daisyui")
-          ],
-=======
   plugins: [require("daisyui")],
->>>>>>> febb7e4480851872c6faa30e8ba9f9315a3017f6
 };
