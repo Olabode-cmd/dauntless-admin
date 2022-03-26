5acd66d32f6c1b969ab317317a029b12ec9023e1
phemieny7 <oyewo.oluwafemi@gmail.com>
2022-03-26 12:20:38 +0100

    a new dawn

--- src/pages/admin/activity.js
+++ src/pages/admin/activity.js
@@ -125,22 +125,39 @@ export default function HomePage() {
                 </div>
 
                 <div className="w-full mt-8 lg:mt-0 lg:w-7/12 lg:pl-4">
-                    <div className="dark:bg-gray-800 bg-gray-300 rounded-3xl px-6 pt-6">
+                    <div className="dark:bg-gray-800 bg-gray-300 rounded-3xl px-6 pt-6 ">
                         <div className="flex text-white text-2xl pb-6 font-bold">
                             <p className='dark:text-gray-100 text-black'>Statisitics</p>
                         </div>
-                        <div>
-                            <div className="card bg-gray-400 dark:bg-gray-600">
+                        <div className="overflow-auto max-h-96">
+                            <div className="card transform transition cursor-pointer hover:-translate-y-2 ml-10 relative flex items-center px-6 py-4 bg-green-300 text-white rounded mb-10 flex-col md:flex-row space-y-4 md:space-y-0">
                                 <div className="p-5">
                                     <p className="mb-2 text-xs font-semibold leading-none tracking-wide text-gray-500 dark:text-white uppercase">Total Income</p>
                                     <h2 className="text-3xl font-extrabold leading-none text-gray-800 dark:text-white truncate">$24,563</h2>
                                 </div>
-                                <a href="#" className="px-5 py-3 text-sm font-medium text-orange-300 card-footer hover:text-black">See all income reports</a>
+                                <a href="#" className="px-5 py-3 text-sm font-medium text-grey-300 card-footer hover:text-black">See all income reports</a>
                             </div>
 
-                            <div className="card bg-gray-400 mt-5">
+                            <div className="card transform transition cursor-pointer hover:-translate-y-2 ml-10 relative flex items-center px-6 py-4 bg-red-300 text-white rounded mb-10 flex-col md:flex-row space-y-4 md:space-y-0">
+
                                 <div className="p-5">
-                                    <p className="mb-2 text-xs font-semibold leading-none tracking-wide text-gray-500 dark:text-white uppercase">Total Income</p>
+                                    <p className="mb-2 text-xs font-semibold leading-none tracking-wide text-gray-500 dark:text-white uppercase">Total users</p>
+                                    <h2 className="text-3xl font-extrabold leading-none text-gray-800 dark:text-white truncate">$24,563</h2>
+                                </div>
+                                <a href="#" className="px-5 py-3 text-sm font-medium text-grey-300 card-footer hover:text-black">See all income reports</a>
+                            </div>
+                            <div className="card transform transition cursor-pointer hover:-translate-y-2 ml-10 relative flex items-center px-6 py-4 bg-orange-300 text-white rounded mb-10 flex-col md:flex-row space-y-4 md:space-y-0">
+
+                                <div className="p-5">
+                                    <p className="mb-2 text-xs font-semibold leading-none tracking-wide text-gray-500 dark:text-white uppercase">Total users</p>
+                                    <h2 className="text-3xl font-extrabold leading-none text-gray-800 dark:text-white truncate">$24,563</h2>
+                                </div>
+                                <a href="#" className="px-5 py-3 text-sm font-medium text-orange-300 card-footer hover:text-black">See all income reports</a>
+                            </div>
+                            <div className="card transform transition cursor-pointer hover:-translate-y-2 ml-10 relative flex items-center px-6 py-4 bg-red-300 text-white rounded mb-10 flex-col md:flex-row space-y-4 md:space-y-0">
+
+                                <div className="p-5">
+                                    <p className="mb-2 text-xs font-semibold leading-none tracking-wide text-gray-500 dark:text-white uppercase">Total users</p>
                                     <h2 className="text-3xl font-extrabold leading-none text-gray-800 dark:text-white truncate">$24,563</h2>
                                 </div>
                                 <a href="#" className="px-5 py-3 text-sm font-medium text-orange-300 card-footer hover:text-black">See all income reports</a>
@@ -154,21 +171,8 @@ export default function HomePage() {
             <div className="flex flex-wrap mt-8">
                 <div className="w-full lg:w-8/12 bg-gray-300 dark:bg-gray-800 py-6 px-6 rounded-3xl">
                     <div className="flex flex-row justify-between">
-                        <p className='dark:text-gray-100 text-black text-2xl pb-6 font-bold'>Trades</p>
-                        <div className="pr-4">
-                            <div className="flex justify-center">
-                                <div>
-                                    {/* <div className="relative inline-flex self-center"> */}
-                                    <select className="text-sm font-bold rounded border-1 border-yellow-700 text-gray-600 h-10 w-60 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none">
-                                        <option value="">View Previous days</option>
-                                        {days.map((day, index) => (
-                                            <option value={index} key={index}>{day}</option>
-                                        ))}
-                                    </select>
-                                    {/* </div> */}
-                                </div>
-                            </div>
-                        </div>
+                        <p className='dark:text-gray-100 text-black text-2xl pb-6 font-bold'>Wallets</p>
+                        
                     </div>
                     <section className="container mx-auto p-6 font-mono">
                         <div className="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
@@ -176,20 +180,20 @@ export default function HomePage() {
                                 <table className="w-full">
                                     <thead>
                                         <tr className="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100  uppercase border-b border-gray-600">
-                                            <th className="px-4 py-3">Reference Id</th>
-                                            <th className="px-4 py-3">Trade Type</th>
+                                            <th className="px-4 py-3">User Id</th>
+                                            <th className="px-4 py-3">Wallet Id</th>
                                             <th className="px-4 py-3">Customer</th>
-                                            <th className="px-4 py-3">Total</th>
+                                            <th className="px-4 py-3">Available Balance</th>
                                             <th className="px-4 py-3">Status</th>
-                                            <th className="px-4 py-3">Date</th>
+                                            <th className="px-4 py-3">Last Date</th>
                                             <th className="px-4 py-3">Action</th>
 
                                         </tr>
                                     </thead>
                                     <tbody className="bg-white">
                                         <tr className="text-gray-700">
-                                            <td className="px-4 py-3 text-ms font-semibold border">Trade001</td>
-                                            <td className="px-4 py-3 text-ms border">Ecode</td>
+                                            <td className="px-4 py-3 text-ms font-semibold border">Daunt001</td>
+                                            <td className="px-4 py-3 text-ms font-semibold border">Wallet001</td>
                                             <td className="px-4 py-3 border">
                                                 <div className="flex items-center text-sm">
                                                     <div className="relative w-8 h-8 mr-3 rounded-full md:block">
@@ -204,7 +208,40 @@ export default function HomePage() {
                                             </td>
                                             <td className="px-4 py-3 text-ms font-semibold border">#20000</td>
                                             <td className="px-4 py-3 text-xs border">
-                                                <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm"> Completed </span>
+                                                <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm"> Active </span>
+                                            </td>
+                                            <td className="px-4 py-3 text-sm border">6/4/2000</td>
+                                            <td className="px-4 py-3 text-sm border">
+                                                <a className="cursor-pointer bg-orange-600 hover:bg-orange-500 text-orange-100 py-2 px-4 rounded inline-flex items-center">
+                                                    <span>
+                                                        <FiEye
+                                                            size={20}
+                                                        />
+                                                    </span>
+                                                </a>
+
+                                            </td>
+
+                                        </tr>
+
+                                        <tr className="text-gray-700">
+                                            <td className="px-4 py-3 text-ms font-semibold border">Daunt002</td>
+                                            <td className="px-4 py-3 text-ms font-semibold border">Wallet002</td>
+                                            <td className="px-4 py-3 border">
+                                                <div className="flex items-center text-sm">
+                                                    <div className="relative w-8 h-8 mr-3 rounded-full md:block">
+                                                        <img className="object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/5212324/pexels-photo-5212324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" loading="lazy" />
+                                                        <div className="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
+                                                    </div>
+                                                    <div>
+                                                        <p className="font-semibold text-black">Sean James</p>
+                                                        <p className="text-xs text-gray-600">Joined a year ago</p>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td className="px-4 py-3 text-ms font-semibold border">#20000</td>
+                                            <td className="px-4 py-3 text-xs border">
+                                                <span className="px-2 py-1 font-semibold leading-tight text-red-700 bg-red-100 rounded-sm"> Banned </span>
                                             </td>
                                             <td className="px-4 py-3 text-sm border">6/4/2000</td>
                                             <td className="px-4 py-3 text-sm border">
