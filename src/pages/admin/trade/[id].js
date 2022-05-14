331e59cd7d4d5d60e1fe4a2ec3783ac55716942b
phemieny7 <oyewo.oluwafemi@gmail.com>
2022-05-14 21:29:12 +0100

    wells

--- src/pages/admin/trade/[id].js
+++ src/pages/admin/trade/[id].js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Breadcumb from '../../../components/breadcumb';
 import AdminLayout from '../../../dashboard/AdminLayout';
 import { Dialog, Transition } from '@headlessui/react'
@@ -200,92 +199,10 @@ export default function HomePage() {
                         <h5 className="text-slate-400 font-bold">Status: <span className="text-slate-100 font-normal">Trade incomplete</span></h5>
 
                         <div className="flex">
-=======
-// import Content from '../components/content';
-import Breadcumb from '../../../components/breadcumb';
-import Statistics from '../../../components/statistics';
-import AreaChart from '../../../components/chart';
-import DoughnutChart from '../../../components/donut';
-import DropdownRender from '../../../components/dropdown';
-import { FiUserPlus, FiDollarSign, FiActivity, FiEye } from 'react-icons/fi'
-import AdminLayout from '../../../dashboard/AdminLayout';
-import { Dialog, Transition } from '@headlessui/react'
-// import {} from 'react'
-import React, { Fragment, useState } from 'react'
-// import "daisyui";
-// import { Helmet } from "react-helmet"
-
-export default function HomePage() {
-    const role = ['seun', 'tope', 'sade',];
-    const days = ["24 hrs ago", "A week ago", "A month ago", "A year ago"];
-
-    const [isOpen, setIsOpen] = useState(false)
-
-
-    return (
-        <AdminLayout>
-
-        <div className="modal" id="my-modal-2">
-          <div className="modal-box">
-            <h3 className="font-bold text-lg text-yellow-400">Query Trade.</h3>
-            <p className="py-4">Kindly say why you want to query this trade.</p>
-            <div className="form">
-                <textarea className="w-full rounded py-2 px-3 resize-none h-36"></textarea>
-            </div>
-            <div className="modal-action flex">
-              <a href="#" className="btn bg-red-600 mx-1 text-slate-100 transition">Cancel</a>
-              <a href="#" className="btn bg-green-600 mx-1 text-slate-100 hover:bg-green-800 transition">Submit</a>
-            </div>
-          </div>
-        </div>
-
-        <div className="modal" id="my-modal-1">
-          <div className="modal-box">
-            <h3 className="font-bold text-lg">Fault Trade</h3>
-            <p className="py-4">Kindly say why you want to fault this trade</p>
-            <div className="form">
-                <textarea className="w-full rounded py-2 px-3 resize-none h-36"></textarea>
-            </div>
-            <div className="modal-action">
-              <a href="#" className="btn bg-red-600 mx-1 text-slate-100 transition">Cancel</a>
-              <a href="#" className="btn bg-green-600 mx-1 text-slate-100 hover:bg-green-800 transition">Submit</a>
-            </div>
-          </div>
-        </div>
-
-      
-
-            <div className="flex flex-wrap">
-                <div className="w-full lg:w-8/12 pt-6 pb-24 bg-gray-300 dark:bg-gray-800 px-6 rounded-3xl">
-                    <Breadcumb title={'Trade'} />
-                    
-                    <div className="flex-col h-full flex justify-between">
-                        <div className="flex flex-wrap">
-                            <div className="w-full lg:w-3/12 mx-3">
-                                <img src="https://images.unsplash.com/photo-1607344645866-009c320b63e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="card image"/>
-                            </div>
-
-                            <div className="w-full lg:w-3/12 mx-3">
-                                <img src="https://images.unsplash.com/photo-1607344645866-009c320b63e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="card image" />
-                            </div>
-
-                            <div className="w-full lg:w-3/12 mx-3">
-                                <img src="https://images.unsplash.com/photo-1607344645866-009c320b63e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="card image" />
-                            </div>
-                        </div>
-
-              {/* <a href="#my-modal-2" className="btn">Open modal</a> */}
-            
-
-                        <h5 className="text-slate-400 font-bold">Status: <span className="text-slate-100 font-normal">Trade incomplete</span></h5>
-
-                        <div className="flex justify-between">
->>>>>>> olabode
                             <button type="button" className="inline-flex mx-1 items-center px-6 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-green-400 border border-transparent rounded-md hover:bg-green-600 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700">
                                 Confirm Trade
                             </button>
                             
-<<<<<<< HEAD
                             <button type="button"
                                 onClick={openModal}
                                  className="inline-flex mx-1 items-center px-6 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-yellow-400 border border-transparent rounded-md hover:bg-yellow-600 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700">
@@ -299,18 +216,6 @@ export default function HomePage() {
                             >
                                 Fault Trade
                             </button>
-=======
-                            <a href="#my-modal-2" type="button"
-                                 className="inline-flex mx-1 items-center px-6 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-yellow-400 border border-transparent rounded-md hover:bg-yellow-600 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700">
-                                Query Trade
-                            </a>
-
-                            <a href="#my-modal-1" type="button"
-                                className="inline-flex mx-1 items-center px-6 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-red-400 border border-transparent rounded-md hover:bg-red-600 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700"
-                            >
-                                Fault Trade
-                            </a>
->>>>>>> olabode
                         </div>
                     </div>
                     
@@ -354,13 +259,6 @@ export default function HomePage() {
                 </div>
 
             </div>
-
-
-           
         </AdminLayout>
     );
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> olabode
