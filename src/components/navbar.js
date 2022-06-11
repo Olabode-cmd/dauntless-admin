a562701b8c3bc9971286837a21aadbc209478fab
phemieny7 <oyewo.oluwafemi@gmail.com>
2022-06-11 17:14:08 +0100

    bode work

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,20 +10,20 @@ export default function Navbar({ login }) {
 
 
     return (
-        <div class="bg-slate-700 fixed z-50 right-px left-px">
-            <div class="px-4 py-2 mx-auto lg:py-4 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-                <div class="relative flex items-center justify-between lg:justify-center lg:space-x-16">
-                    <ul class="flex items-center hidden space-x-8 lg:flex">
+        <div className="bg-slate-700 fixed z-50 right-px left-px">
+            <div className="px-4 py-2 mx-auto lg:py-4 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+                <div className="relative flex items-center justify-between lg:justify-center lg:space-x-16">
+                    <ul className="flex items-center hidden space-x-8 lg:flex">
                         <li>
                             <Link
                                 href="/">
-                                <a class="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400">Home</a>
+                                <a className="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400">Home</a>
                             </Link>
                         </li>
                         <li>
                             <Link
                                 href="/about">
-                                <a class="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400"> About Us</a>
+                                <a className="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400"> About Us</a>
                             </Link>
                         </li>
                         <li>
@@ -31,18 +31,18 @@ export default function Navbar({ login }) {
                                 href="https://dauntless.tawk.help/">
                                 
                                 <a target="_blank"
-                                    class="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400">
+                                    className="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400">
                                     Help Center</a>
                             </Link>
                         </li>
                     </ul>
                     <img src="/images/2.png" width={45} />
-                    <ul class="flex items-center hidden space-x-8 lg:flex">
+                    <ul className="flex items-center hidden space-x-8 lg:flex">
                         <li>
                             <Link
                                 href="https://dauntless.tawk.help/category/faq" >
                                 <a target="_blank"
-                                    class="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400">
+                                    className="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400">
                                     FAQs
                                 </a>
                             </Link>
@@ -50,24 +50,24 @@ export default function Navbar({ login }) {
                         <li>
                             <Link
                                 href="/auth/login">
-                                <a class="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400">Sign in</a>
+                                <a className="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400">Sign in</a>
                             </Link>
                         </li>
                         <li>
                             <Link
                                 href="/">
-                                <a class="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400">Sign up</a>
+                                <a className="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400">Sign up</a>
                             </Link>
                         </li>
                     </ul>
-                    <div class="lg:hidden">
+                    <div className="lg:hidden">
                         <button
                             aria-label="Open Menu"
                             title="Open Menu"
-                            class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
+                            className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
                             onClick={() => setIsMenuOpen(true)}
                         >
-                            <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                            <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
                                 <path
                                     fill="currentColor"
                                     d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
@@ -83,9 +83,9 @@ export default function Navbar({ login }) {
                             </svg>
                         </button>
                         {isMenuOpen && (
-                            <div class="absolute top-0 left-0 w-full">
-                                <div class="p-5 bg-white border rounded shadow-sm">
-                                    <div class="flex items-center justify-between mb-4">
+                            <div className="absolute top-0 left-0 w-full">
+                                <div className="p-5 bg-white border rounded shadow-sm">
+                                    <div className="flex items-center justify-between mb-4">
                                         <div>
                                             <img src="/images/2.png" width={30} />
                                         </div>
@@ -93,10 +93,10 @@ export default function Navbar({ login }) {
                                             <button
                                                 aria-label="Close Menu"
                                                 title="Close Menu"
-                                                class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                                                className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                                                 onClick={() => setIsMenuOpen(false)}
                                             >
-                                                <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                                                <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
                                                     <path
                                                         fill="currentColor"
                                                         d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
@@ -106,17 +106,17 @@ export default function Navbar({ login }) {
                                         </div>
                                     </div>
                                     <nav>
-                                        <ul class="space-y-4">
+                                        <ul className="space-y-4">
                                             <li>
                                                 <Link
                                                     href="/">
-                                                    <a class="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400">Home</a>
+                                                    <a className="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400">Home</a>
                                                 </Link>
                                             </li>
                                             <li>
                                                 <Link
                                                     href="/about">
-                                                    <a class="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400"> About Us</a>
+                                                    <a className="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400"> About Us</a>
                                                 </Link>
                                             </li>
                                             <li>
@@ -124,7 +124,7 @@ export default function Navbar({ login }) {
                                                     href="https://dauntless.tawk.help/">
 
                                                     <a target="_blank"
-                                                        class="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400">
+                                                        className="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400">
                                                         Help Center</a>
                                                 </Link>
                                             </li>
@@ -132,7 +132,7 @@ export default function Navbar({ login }) {
                                                 <Link
                                                     href="https://dauntless.tawk.help/category/faq" >
                                                     <a target="_blank"
-                                                        class="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400">
+                                                        className="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400">
                                                         FAQs
                                                     </a>
                                                 </Link>
@@ -140,13 +140,13 @@ export default function Navbar({ login }) {
                                             <li>
                                                 <Link
                                                     href="/auth/login">
-                                                    <a class="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400">Sign in</a>
+                                                    <a className="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400">Sign in</a>
                                                 </Link>
                                             </li>
                                             <li>
                                                 <Link
                                                     href="/">
-                                                    <a class="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400">Sign up</a>
+                                                    <a className="text-sm tracking-wide text-white transition-colors duration-200 hover:text-teal-accent-400">Sign up</a>
                                                 </Link>
                                             </li>
                                         </ul>
