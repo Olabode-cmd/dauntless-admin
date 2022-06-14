109e0128cc0f5f79784f4e01ec707514f8d901e9
phemieny7 <oyewo.oluwafemi@gmail.com>
2022-06-14 17:55:05 +0100

    everyday is a step closer

--- src/pages/admin/_middleware.js
+++ src/pages/admin/_middleware.js
@@ -5,14 +5,12 @@ import { NextResponse } from 'next/server';
 export async function middleware(req) {
     const session = await getToken({ req, secret: process.env.SECRET })
     if (session && session?.user?.role == 1) {
-        console.log('i worked')
         return NextResponse.next();
     }
     // the user is not logged in, redirect to the sign-in page
-    const signInPage = '/auth/logout';
+    const signInPage = '/404';
     const signInUrl = new URL(signInPage, req.nextUrl.origin);
     signInUrl.searchParams.append('callbackUrl', req.url);
-    console.log('admin is not for you')
 return NextResponse.redirect(signInUrl);
 
 }
